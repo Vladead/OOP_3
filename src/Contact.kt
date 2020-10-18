61b378b0533794b77edf9d44fb3aa14d6fd44b59
Влад Концевых <vladkontsevykh@list.ru>
2020-10-18 13:07:31 +0300

    Contact: number adding/removing/editing

--- src/Contact.kt
+++ src/Contact.kt
@@ -1,17 +1,17 @@
 import kotlin.collections.HashMap
-import kotlin.reflect.jvm.internal.impl.util.ValueParameterCountCheck
 
 class Contact {
     var name: String = String()
 
     private val numbers: MutableMap<String, NumType> = HashMap()
+        get() = field.toMutableMap()
 
     constructor(newName: String, newNumber: String, type: NumType) {
         name = newName
         numbers[newNumber] = type
     }
 
-    fun contains(subString: String) : Boolean {
+    fun contains(subString: String): Boolean {
         if (name.contains(subString))
             return true
         for (i in numbers)
@@ -21,23 +21,52 @@ class Contact {
     }
 
     fun addNumber(number: String, type: NumType) {
-        TODO("Влад = потом")
+        if (number.isNotEmpty()) {
+            if (numbers.containsKey(number))
+                throw IllegalArgumentException("Number already added!")
+            else {
+                val hasPlus: Int = (number[0] == '+').int
+                if (number.substring(hasPlus..number.length).toLongOrNull() == null)
+                    throw IllegalArgumentException("Incorrect number format")
+                if (number.length > 11 + hasPlus)
+                    throw IllegalArgumentException("Nonexistent number")
+                numbers[number] = type
+            }
+        } else
+            throw IllegalArgumentException("Received empty number!")
     }
 
-    fun removeNumber(number: String) {
-        TODO("Влад = потом")
+    fun removeNumber(number: String): Boolean {
+        return numbers.remove(number) != null
     }
 
-    fun editNumber(numberToEdit: String, newNumber: String? = null, newType: NumType? = null) {
-        TODO("Влад = потом")
+    fun editNumber(numberToEdit: String, newNumber: String = String(), newType: NumType? = null) {
+        editNumber(numberToEdit, newNumber)
+        editNumber(numberToEdit, newType)
     }
 
-    fun editNumber(numberToEdit: String, newNumber: String? = null) {
-        TODO("Влад = потом")
+    fun editNumber(numberToEdit: String, newNumber: String = String()) {
+        if (newNumber.isNotEmpty()) {
+            if (numbers.containsKey(newNumber))
+                throw IllegalArgumentException("Number already added!")
+            else {
+                val hasPlus: Int = (newNumber[0] == '+').int
+                if (newNumber.substring(hasPlus..newNumber.length).toLongOrNull() == null)
+                    throw IllegalArgumentException("Incorrect number format")
+                if (newNumber.length > 11 + hasPlus)
+                    throw IllegalArgumentException("Nonexistent number")
+
+                val type = numbers.remove(numberToEdit)
+                        ?: throw IllegalArgumentException("Replacing nonexistent number!")
+
+                numbers[newNumber] = type
+            }
+        } else
+            throw IllegalArgumentException("Received empty number!")
     }
 
     fun editNumber(numberToEdit: String, newType: NumType? = null) {
-        TODO("Влад = потом")
+        numbers[numberToEdit] = newType ?: throw IllegalArgumentException("Didn't receive type!")
     }
 
     override fun equals(other: Any?): Boolean {
@@ -54,4 +83,7 @@ class Contact {
         TODO("Влад = потом")
         return super.toString()
     }
+
+    private val Boolean.int
+        get() = if (this) 1 else 0
 }
